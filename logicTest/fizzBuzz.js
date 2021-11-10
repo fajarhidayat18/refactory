7034c7e73178a5ea5103b5a35dab6fcdcc20b346
fajarhidayat18 <fajarh085@gmail.com>
2021-11-10 19:08:21 +0700

    fizzbuzz

---
+++ logicTest/fizzBuzz.js
@@ -0,0 +1,16 @@
+// logika
+function fizzbuzz(number) {
+    for (let i = 1; i < number + 1; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("Fizz Buzz")
+        } else if (i % 3 == 0) {
+            console.log("Fizz")
+        } else if (i % 5 == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+// hasil
+fizzbuzz(30)
